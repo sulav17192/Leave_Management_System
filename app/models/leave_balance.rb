7f34ca3dcebe85be09cd9bf3ee0c421738985faa
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-04 17:01:21 +0545

    Added the leave_type associaltion to other models

--- app/models/leave_balance.rb
+++ app/models/leave_balance.rb
@@ -1,6 +1,7 @@
 class LeaveBalance < ApplicationRecord
   belongs_to :employee
-
+  belongs_to :leave_type
+  
   validates :leave_type, presence: true
   validates :balance_days, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/leave_request.rb
+++ app/models/leave_request.rb
@@ -1,5 +1,6 @@
 class LeaveRequest < ApplicationRecord
   belongs_to :employee
+  belongs_to :leave_type
 
   validates :leave_type, :start_date, :end_date, presence: true
   validates :status, presence: true
