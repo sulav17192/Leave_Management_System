3918b3ebcfccc776a8a98c598b274db6a6412a4e
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-04 17:04:10 +0545

    Added call back in leave_request model

--- app/models/leave_request.rb
+++ app/models/leave_request.rb
@@ -4,4 +4,12 @@ class LeaveRequest < ApplicationRecord
 
   validates :leave_type, :start_date, :end_date, presence: true
   validates :status, presence: true
+
+  before_create :set_default_status
+
+  private
+
+  def set_default_status
+    self.status ||= "pending"
+  end
 end
